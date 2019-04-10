d858327db19c5e9f77d94a3b9396bf7e4d61c24c
Qiu Cheng <qiucheng@sightp.com>
2019-04-10 19:10:17 +0800

    start hotFix

---
+++ Assets/Scripts/HotFixScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using XLua;
+
+public class HotFixScript : MonoBehaviour {
+
+    private LuaEnv luaEnv;
+
+    void Start () {
+        luaEnv = new LuaEnv();
+
+        luaEnv.AddLoader(MyLoader);
+
+        luaEnv.DoString("require'fish'");
+    }
+
+    private byte[] MyLoader(ref string filePath)
+    {
+        string absPath = Path.Combine(Application.dataPath , "..") + "/PlayerGamePackage/" + filePath + ".lua.txt";
+        //return File.ReadAllBytes(absPath);
+        return System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(absPath));
+    }
+}
